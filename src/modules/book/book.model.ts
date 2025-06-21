6cf2f1e2879f9ad0f71c7302942518bbad9d409e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-21 15:56:48 +0600

    feat: define book model schema with properties and export model

--- src/modules/book/book.model.ts
+++ src/modules/book/book.model.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IBook } from "./book.interface";
 
 const bookSchema = new Schema<IBook>({
@@ -7,4 +7,36 @@ const bookSchema = new Schema<IBook>({
     required: true,
     trim: true,
   },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  genre: {
+    type: String,
+    enum: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "BIOGRAPHY", "FANTASY"],
+    required: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  copies: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
 });
+
+const Book = model<IBook>("Book", bookSchema);
+
+export default Book;
