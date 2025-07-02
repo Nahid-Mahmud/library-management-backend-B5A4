dab6d0635b8d2572bbff8a51ae8d81bb261c72b4
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-02 13:17:34 +0600

    refactor: add Meta interface to sendResponse for enhanced response structure

--- src/helper/sendResponse.ts
+++ src/helper/sendResponse.ts
@@ -1,5 +1,18 @@
 import { Response } from "express";
 
-export const sendResponse = <T>(res: Response, statusCode: number, success: boolean, message: string, data?: T) => {
-  res.status(statusCode).json({ success, message, data });
+interface Meta {
+  page?: number;
+  limit?: number;
+  total?: number;
+}
+
+export const sendResponse = <T>(
+  res: Response,
+  statusCode: number,
+  success: boolean,
+  message: string,
+  meta?: Meta,
+  data?: T
+) => {
+  res.status(statusCode).json({ success, message, data, meta });
 };
